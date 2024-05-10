6a945db2c302991ee46f3ad08099c969b2280ab8
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-10 07:47:24 +0530

    feat: add country list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/card/Card';
 
 function App() {
-    return <>Welcome</>;
+    const [countries, setCountries] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+
+    const fetchCountries = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            const countryList = await response.json();
+            setCountries(() => [...countryList]);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchCountries();
+    }, []);
+    return (
+        <section className='main-container'>
+            {isLoading ? (
+                <span>Loading...</span>
+            ) : (
+                countries.map((country) => (
+                    <section className='cards' key={country.name.common}>
+                        <Card flag={country.flags.png} name={country.name.common} alt={country.flags.alt} />
+                    </section>
+                ))
+            )}
+        </section>
+    );
 }
 
 export default App;
